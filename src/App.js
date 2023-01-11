26d832b951f5a1cd587f71f91a3c0041f2742e7d
Gankhulug02 <huluguu0202@gmail.com>
2023-01-11 17:27:44 +0800

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { SocialIcon } from "react-social-icons";
+import { Typewriter } from "react-simple-typewriter";
+import { motion } from "framer-motion";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <motion.div
+          initial={{ x: -500 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 5 }}
+        >
+          <SocialIcon network="youtube" className="App-logo" />
+          <SocialIcon network="facebook" className="App-logo" />
+          <SocialIcon network="twitter" className="App-logo" />
+          <SocialIcon network="instagram" className="App-logo" />
+        </motion.div>
+        <Typewriter words={["Pinecone", "Work", "Smart"]} />
+        <motion.div layout />
         <a
           className="App-link"
           href="https://reactjs.org"
